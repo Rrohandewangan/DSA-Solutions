43ef0f355f14804c6dd9f117c4db61b2634392ef
Rrohandewangan <dewanganrohan126@gmail.com>
2026-02-23 12:37:09 +0530

    Fix rotation logic for word reversal

--- Strings/RotateWordInString.java
+++ Strings/RotateWordInString.java
@@ -1,26 +1,44 @@
 public class RotateWordInString {
+
+  //https://leetcode.com/problems/reverse-words-in-a-string/description/
+
     
-     public static String reverseWords(String s) { 
-      int n = s.length() ;
-      StringBuilder sb = new StringBuilder(s) ;
-      String ans = "" ;
+     public static String reverseWords(String s) {
+        int n = s.length() ;
+        StringBuilder sb = new StringBuilder(s) ;
+        StringBuilder ans = new StringBuilder("") ;
 
-      sb.reverse() ;
+        sb.reverse() ; // to reverse given string 
 
-      for(int i=0; i<n; i++) {
-        StringBuilder word = new StringBuilder("") ;
+        for(int i=0; i<n; i++) {
+            StringBuilder word = new StringBuilder("") ; // to store each word of string
+            
         while(i < n && sb.charAt(i) != ' ') {
               word.append(sb.charAt(i)) ;
               i++ ;
         }
 
-         word.reverse() ;
+      // method 1 ->
+        // word.reverse() ; // to reverse each word come from after while loop runs.
+
+        // if(word.length() > 0) {
+        //     ans.append(" " + word) ;
+        // }
+        // }
+        // ans.toString() ;
+
+        // return ans.substring(1) ;
+
+       // method 2 ->
         if(word.length() > 0) {
-           ans += " "+ word ;
-        }
-      }
-        return ans.substring(1) ;
-     }
+            if(ans.length() > 0) {
+                ans.append(' ');
+            }
+            ans.append(word.reverse()) ;
+        }  
+    }
+    return ans.toString() ;
+ } 
 
     public static void main(String[] args) {
       String s = "the sky is blue" ;
