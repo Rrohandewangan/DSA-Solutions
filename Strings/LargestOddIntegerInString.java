aafa558cc07ae53d006bca2b8503aa110fb38cc0
Rrohandewangan <dewanganrohan126@gmail.com>
2026-02-23 13:17:15 +0530

    New logic for check odd number in string

--- Strings/LargestOddIntegerInString.java
+++ Strings/LargestOddIntegerInString.java
@@ -1,15 +1,13 @@
 public class LargestOddIntegerInString {
     
-    public static String largestOddNumber(String num) {
-        int ans = 0 ;
-        for(int i=num.length()-1; i>=0; i--) {
-            ans = num.charAt(i) - '0' ;
-            if(ans % 2 != 0) {
-                return num.substring(0, i+1) ;
-            } 
-        }
-        return "" ;
-        
+   public static String largestOddNumber(String num) {
+         int n = num.length() ;
+         for(int i=n-1; i>=0; i--) { // we can ilitrate from right to left because if first number of right is odd then whole number is odd number and if not then skip it.
+            if((int) num.charAt(i) % 2 != 0) { // we cast it to an integer and check it.
+                return num.substring(0, i+1) ; // here i+1 because it is exclude last index in substring function.
+            }
+         }
+         return "" ;
     }
     public static void main(String[] args) {
        String num = "4206" ;
