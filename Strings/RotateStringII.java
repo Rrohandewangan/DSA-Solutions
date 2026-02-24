82bd7a607cca975dae95951635448a143e365108
Rrohandewangan <dewanganrohan126@gmail.com>
2026-02-24 15:00:44 +0530

    add brute force solution

--- Strings/RotateStringII.java
+++ Strings/RotateStringII.java
@@ -1,5 +1,27 @@
 public class RotateStringII {
+
+   // Brute force approach ->
+   
+    // TC -> O(n2)
+    // SC -> O(n)
+   //  public boolean rotateString(String s, String goal) {
+   //      if(s.length() != goal.length()) {
+   //          return false ;
+   //      }
+
+   //      for(int i=0; i<s.length(); i++) { //O(n)
+   //          String rotated = s.substring(i) + s.substring(0, i) ; //O(n) every time creates string of length n. 
+   //          // check rotated is equal to goal or not
+   //          if(rotated.equals(goal)) {
+   //              return true ;
+   //          }
+   //      }
+   //      return false ;
+   //  }
     
+
+   // optimal approach -> 
+   
    public static boolean rotateString(String s, String goal) {  //TC -> O(1)
      if(s.length() != goal.length()) { // here rotation is not possible
         return false ;
