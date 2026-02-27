d6c3d12b21acd3e66c99969c6ec21f3715280aed
Rrohandewangan <dewanganrohan126@gmail.com>
2026-02-27 16:20:24 +0530

    TwoSum problem

--- Arrays/Twosum.java
+++ Arrays/Twosum.java
@@ -2,18 +2,49 @@ import java.util.* ;
 
 public class Twosum {
 
-  public static int[] twoSum(int[] nums, int target) {
+  // brute force approach -> 
+  // TC -> O(n2)
+  // SC -> O(1)
+  // public static int[] twoSum(int[] nums, int target) {
     
-    for(int i=0; i<nums.length-1; i++) {
-      for(int j=i+1; j<nums.length; j++) {
-        if(nums[i] + nums[j] == target) {
-          return new int[]{i, j} ;
-        } 
+  //   for(int i=0; i<nums.length-1; i++) {
+  //     for(int j=i+1; j<nums.length; j++) {
+  //       if(nums[i] + nums[j] == target) {
+  //         return new int[]{i, j} ;
+  //       } 
         
-      }
-    }
-     return new int[]{} ;
-  }
+  //     }
+  //   }
+  //    return new int[]{} ;
+  // }
+
+
+  // better approach -> using sorting and store element of array with its real index in other data structure.
+  // TC -> O(nlogn)
+  // SC -> O(n)
+  // i am only learn this solution but dont code it do it by yourself...
+
+  
+
+  // optimal solution -> using hashmap
+  // TC -> O(n)
+  // SC -> O(n)
+  // public int[] twoSum(int[] nums, int target) {
+  //        HashMap<Integer, Integer> map = new HashMap<>() ;
+
+  //        // run a loop ->
+  //        for(int i=0; i<nums.length; i++) {
+  //           int first = nums[i] ;
+  //           int second = target - first ;
+
+  //           if(map.containsKey(second)) {
+  //               return new int[]{map.get(second), i} ;
+  //           } else {
+  //               map.put(nums[i], i) ;
+  //           }
+  //        }
+  //        return new int[]{} ;
+  //   }
     public static void main(String[] args) {
 
       Scanner sc = new Scanner(System.in) ;
