90d5028718946e831c181612e73691c27f8bb9c2
Rrohandewangan <dewanganrohan126@gmail.com>
2026-03-18 13:10:16 +0530

    permutations question

---
+++ Recursion/Problems/Permutations2.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations2 {
+    
+    // TC -> O(n! * n)
+    // SC -> O(n! * n)
+    static void getPerms(int[] nums, int idx, List<List<Integer>> ans) {
+         if(idx == nums.length) {
+            ArrayList<Integer> curr = new ArrayList<>() ;
+            for(int num : nums) {
+                curr.add(num) ;
+            }
+            ans.add(curr) ;
+            return ;
+         }
+
+         for(int i=idx; i<nums.length; i++) {
+            //swap => idx place => ith element choice
+            int temp = nums[idx] ;
+            nums[idx] = nums[i] ;
+            nums[i] = temp ;
+            getPerms(nums, idx+1, ans) ;
+
+            //backtracking step
+            temp = nums[idx] ;
+            nums[idx] = nums[i] ;
+            nums[i] = temp ;
+         }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>() ;
+        getPerms(nums, 0, ans) ;
+        return ans ;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
