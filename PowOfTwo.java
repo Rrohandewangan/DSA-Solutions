b4140e85f9f42453162e4fe7e5b87cfe82954772
Rrohandewangan <dewanganrohan126@gmail.com>
2026-03-25 12:53:23 +0530

    Recursion easy questions

---
+++ PowOfTwo.java
@@ -0,0 +1,19 @@
+public class PowOfTwo {
+    
+    // TC -> o(logn)
+    // SC -> o(logn)
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1) {
+            return true ;
+        }
+        
+        if(n < 1 || n % 2 != 0) {
+            return false ;
+        }
+
+        return isPowerOfTwo(n/2) ;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
