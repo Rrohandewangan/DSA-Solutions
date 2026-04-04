a0680ef3ca5c1032b5e3bf3c99bd640e95cf10c1
Rrohandewangan <dewanganrohan126@gmail.com>
2026-04-04 15:14:51 +0530

    new recursion question

---
+++ KthSymbolnGrammer.java
@@ -0,0 +1,23 @@
+public class KthSymbolnGrammer {
+    
+    // TC -> O(n)
+    // SC -> O(n)
+    public int kthGrammar(int n, int k) {
+        if(n == 1) {
+            return 0 ;
+        }
+
+        int len = 1 << (n - 1) ;
+        int mid = len / 2 ;
+
+        if(k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return 1 - kthGrammar(n - 1, k - mid) ;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
