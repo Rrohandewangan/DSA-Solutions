5b99cbe81aadf50c39c266a14ef454f8f34c7f63
Rrohandewangan <dewanganrohan126@gmail.com>
2026-05-22 01:01:40 +0530

    Stack Hard problem

--- Stack/Problems/LargestRectangleInHistogram.java
+++ Stack/Problems/LargestRectangleInHistogram.java
@@ -34,6 +34,50 @@ public class LargestRectangleInHistogram {
         }
         return Math.max(max, area);
     }
+
+
+    // Method - 2 
+    // TC -> O(n)
+    // SC -> O(n)
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n]; //left smaller nearest 
+        int[] right = new int[n]; // right smaller nearest
+        Stack<Integer> s = new Stack<>();
+
+        //Right Smaller Nearest ->
+        for(int i=n-1; i>=0; i--) {
+            while(!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+
+            right[i] = s.isEmpty() ? n : s.peek();
+            s.push(i);
+        }
+
+        while(!s.isEmpty()) {
+            s.pop();
+        }
+
+        //Left Smaller Nearest -> 
+        for(int i=0; i<n; i++) {
+            while(!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+
+            left[i] = s.isEmpty() ? -1 : s.peek();
+            s.push(i);
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            int currArea = heights[i] * width;
+            ans = Math.max(ans, currArea);
+        }
+        
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
