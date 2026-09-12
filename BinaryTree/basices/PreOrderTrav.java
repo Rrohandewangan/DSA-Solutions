4f516db508fc63c8cd61453340bd8e051338a542
Rrohandewangan <dewanganrohan126@gmail.com>
2026-09-12 14:19:02 +0530

    add BinaryTree folder

---
+++ BinaryTree/basices/PreOrderTrav.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+        
+    public static void preorder(Node root, List<Integer> arr) {
+        if(root == null) {
+            return;
+        }
+
+        arr.add(root.data);
+        preorder(root.left, arr);
+        preorder(root.right, arr);
+
+    }
+
+    public List<Integer> preOrder(Node root) {
+            
+        List<Integer> arr = new ArrayList<>();
+
+        preorder(root, arr);
+
+        return arr;
+    }
+}
+
+public class PreOrderTrav {
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        Solution sol = new Solution();
+        List<Integer> result = sol.preOrder(root);
+
+        System.out.println("Preorder Traversal :");
+
+        for(int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ BinaryTree/basices/tree.java
@@ -0,0 +1,23 @@
+public class tree {
+    
+    static class Node {
+        int data; 
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+    }
+}
